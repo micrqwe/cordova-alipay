f8f90a5c1365045873cf530b80ee1e8e7046cf7a
swx <287507016@qq.com>
2016-11-23 18:18:44 +0800

    添加插件

--- src/ios/AlipayPlugin.m
+++ src/ios/AlipayPlugin.m
@@ -1,5 +1,4 @@
 #import "AlipayPlugin.h"
-#import "Order.h"
 
 #import "DataSigner.h"
 #import <AlipaySDK/AlipaySDK.h>
@@ -8,9 +7,6 @@
 
 -(void)pluginInitialize{
     CDVViewController *viewController = (CDVViewController *)self.viewController;
-    self.partner = [viewController.settings objectForKey:@"partner"];
-    self.seller = [viewController.settings objectForKey:@"seller"];
-    self.privateKey = [viewController.settings objectForKey:@"privatekey"];
 }
 
 - (void) pay:(CDVInvokedUrlCommand*)command
@@ -21,21 +17,6 @@
      *商户的唯一的parnter和seller。
      *签约后，支付宝会为每个商户分配一个唯一的 parnter 和 seller。
      */
-    
-    //partner和seller获取失败,提示
-    if ([self.partner length] == 0 ||
-        [self.seller length] == 0 ||
-        [self.privateKey length] == 0)
-    {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"提示"
-                                                        message:@"缺少partner或者seller或者私钥。"
-                                                       delegate:self
-                                              cancelButtonTitle:@"确定"
-                                              otherButtonTitles:nil];
-        [alert show];
-        return;
-    }
-    
     /*
      *生成订单信息及签名
      */
@@ -54,54 +35,6 @@
         
         NSLog(@"reslut = %@",resultDic);
     }];
-    
-    //    NSString   *tradeId  = [args objectForKey:@"tradeNo"];
-    //    NSString   *subject  = [args objectForKey:@"subject"];
-    //    NSString   *body     = [args objectForKey:@"body"];
-    //    NSString   *price    = [args objectForKey:@"price"];
-    //    NSString   *notifyUrl    = [args objectForKey:@"notifyUrl"];
-    //
-    //    Order *order = [[Order alloc] init];
-    //    order.partner = self.partner;
-    //    order.seller = self.seller;
-    //    order.tradeNO = tradeId; //订单ID（由商家自行制定）
-    //    order.productName = subject; //商品标题
-    //    order.productDescription = body; //商品描述
-    //    order.amount = price; //商品价格
-    //    order.notifyURL =  notifyUrl; //回调URL
-    //
-    //    order.service = @"mobile.securitypay.pay";
-    //    order.paymentType = @"1";
-    //    order.inputCharset = @"utf-8";
-    //    order.itBPay = @"30m";
-    //    order.showUrl = @"m.alipay.com";
-    //
-    //    //将商品信息拼接成字符串
-    //    NSString *orderSpec = [order description];
-    //    NSLog(@"orderSpec = %@",orderSpec);
-    //
-    //    //获取私钥并将商户信息签名,外部商户可以根据情况存放私钥和签名,只需要遵循RSA签名规范,并将签名字符串base64编码和UrlEncode
-    //    id<DataSigner> signer = CreateRSADataSigner(self.privateKey);
-    //    NSString *signedString = [signer signString:orderSpec];
-    //
-    //    //将签名成功字符串格式化为订单字符串,请严格按照该格式
-    //    NSString *orderString = nil;
-    //    if (signedString != nil) {
-    //        orderString = [NSString stringWithFormat:@"%@&sign=\"%@\"&sign_type=\"%@\"",
-    //                       orderSpec, signedString, @"RSA"];
-    //
-    //
-    //        [[AlipaySDK defaultService] payOrder:orderString fromScheme:[NSString stringWithFormat:@"a%@", self.partner] callback:^(NSDictionary *resultDic) {
-    //            if ([[resultDic objectForKey:@"resultStatus"]  isEqual: @"9000"]) {
-    //                [self successWithCallbackID:self.currentCallbackId messageAsDictionary:resultDic];
-    //            } else {
-    //                [self failWithCallbackID:self.currentCallbackId messageAsDictionary:resultDic];
-    //            }
-    //
-    //            NSLog(@"reslut = %@",resultDic);
-    //        }];
-    //
-    //    }
 }
 
 - (void)handleOpenURL:(NSNotification *)notification
